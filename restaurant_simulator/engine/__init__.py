611ae0446bcdeaee619ce7cd48ac589fd2c6c4dd
Daniil Akhramiuk <danil.bel53@gmail.com>
2026-05-24 13:52:37 +0300

    feat: add tick processing engine

--- restaurant_simulator/engine/__init__.py
+++ restaurant_simulator/engine/__init__.py
@@ -1,7 +1,9 @@
+from .tick import process_tick, run_ticks
 from .spawning import spawn_guest, should_spawn_guest
 from .events import RandomEvent, RANDOM_EVENTS, pick_random_event
 
 __all__ = [
+    "process_tick", "run_ticks",
     "spawn_guest", "should_spawn_guest",
     "RandomEvent", "RANDOM_EVENTS", "pick_random_event",
 ]
